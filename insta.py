1fc8a5240e34372f8a10661dbd72afd2a9f0ebd8
Merakulax <68378642+Merakulax@users.noreply.github.com>
2022-03-15 13:26:04 +0100

    nun gibt er etwas aus, damit man schonmal den rest ausprobieren kann.

--- insta.py
+++ insta.py
@@ -12,7 +12,7 @@ def main():
 
     csv_file = open('beispiel.csv', 'w')
     csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['posting_url ', 'profile_url ', 'posted_date ', 'num_likes ', 'num_views ', 'posting_text'])
+    csv_writer.writerow(['posting_url', 'profile_url', 'posted_date', 'num_likes', 'num_views','posting_text'])
 
     # Get Html and posting_url also which post? .
     for index, row in df.iterrows():
@@ -39,19 +39,23 @@ def main():
 
         # try getting the likes -> if post
         try:
-            likes = soup.find('div', class_="_7UhW9 xLCgt qyrsm KV-D4 fDxYl T0kll").span.text
+            num_likes = soup.find('div', class_="_7UhW9 xLCgt qyrsm KV-D4 fDxYl T0kll").span.text
 
         # Else it should be a video -> get the views
-        except TypeError:
-            views = soup.find_all('div', class_="_7UhW9 xLCgt qyrsm KV-D4 uL8Hv T0kll")[1].text[:-8]
+        except AttributeError:
+            num_views = soup.find_all('div', class_="_7UhW9 xLCgt qyrsm KV-D4 uL8Hv T0kll")[1].text[:-8]
+
 
         # Get the posting text
         posting_text = soup.find('span', class_="_7UhW9 xLCgt MMzan KV-D4 se6yk T0kll").text
         print(posting_text)
 
-        csv_writer.writerow([posting_url, profile_url, posted_date, num_likes, num_views, posting_text])
-
-        csv_file.close()
+        try:
+            csv_writer.writerow([posting_url, profile_url, posted_date, num_likes, num_views, posting_text])
+        except UnicodeEncodeError:
+            print("!!!hey, da hat was mit dem encoden nicht funktioniert")
+            csv_writer.writerow([posting_url, profile_url, posted_date, num_likes, num_views, None])
+    csv_file.close()
     return None
 
 
